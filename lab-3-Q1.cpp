b143156734e070de5ddb7241c905b7c73c87363c
ALI ZIA KHAN <azk443.443@gmail.com>
2020-07-14 03:24:47 +0500

    lab-task-3-Q1     factorial of a number using pointer via function

---
+++ lab-3-Q1.cpp
@@ -0,0 +1,36 @@
+/*factorial of a number*/
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int factorial( int *pt1){
+   
+   int fact=1;
+   for(int i=1;i<=*pt1;i++){
+   	
+   	fact=fact*i;
+   }
+	return fact;
+		
+}
+
+
+
+
+int main(){
+  int a;
+  int *pt1;
+  cout<<"Enter no please to claculate factorial"<<endl;
+  cin>>a;
+  pt1=&a;
+  cout<<"The factorial of : " <<*pt1<<" is---------- "<<a<<" != "<<factorial(&a)<<endl;
+}
+  	
+
+
+
+/*output
+Enter no please to claculate factorial
+5
+The factorial of : 5 is---------- 5 != 120
+*/
